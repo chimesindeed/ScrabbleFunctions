99d50d0a863941f3bf04541b09e781227520bc64
dimitri.gedda <gratitudeadore@fastmail.com>
2021-03-18 22:56:13 -0400

    variables holding Scrabble bag of letters and Scrabble letter point values

---
+++ scrabble-variables.js
@@ -0,0 +1,7 @@
+// blank,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
+export let scrabbleBag = [2,9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1]
+
+export const letterPointValues = { 
+  a: 1, b: 3, c: 3,  d: 2, e: 1, f: 4, g: 2, h: 4, i: 1, j: 8, k: 5, l: 1, m: 3, n: 1,
+  o: 1, p: 3, q: 10, r: 1, s: 1, t: 1, u: 1, v: 4, w: 4, x: 8, y: 4, z: 10,
+}
